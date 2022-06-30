9bcedde4e0d87383b23d4d2003aa4ca9934bf2bc
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-06-30 17:27:14 -0300

    commit operadores

---
+++ Atv04Operadores/Atv04Operadores/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 10;
+Console.WriteLine(a);
+a += 10;
+Console.WriteLine(a);
+a++;
+Console.WriteLine(a);
+a--;
+Console.WriteLine(a);
+a *= 2;
+Console.WriteLine(a);
+string alf = "ABC";
+Console.WriteLine(alf);
+alf += "DEF";
+Console.WriteLine(alf);
+
+ a = 10;
+int b = a++;
+Console.WriteLine(a);
+Console.WriteLine("B com o mais pro lado dir: {0}", b);
+
+
+a = 10;
+b = ++a;
+Console.WriteLine("B com o mais pro lado esq: {0}", b);
+
+
