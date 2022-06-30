8bd618b24ec23bcc95915b5e45c6ff6d9c0e2f41
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-06-30 16:55:14 -0300

    commit

--- Ex1SaidaDeDados/Ex1SaidaDeDados/Program.cs
+++ Ex1SaidaDeDados/Ex1SaidaDeDados/Program.cs
@@ -3,8 +3,29 @@ using System.Globalization;
 
 
 namespace Ex1 {
-    class Ex1 {
+    class Program {
+
         static void Main(string[] args) {
+            string produto1 = "computador";
+            string produto2 = "mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: \nComputador, cujo preço é $ " + preco1 + "\nMesa de escritorio, cujo preço é $ " + preco2);
+            Console.WriteLine("\n\nRegistros: {0} anos de idade, Codigo {1} e Genero: {2}.", idade, codigo, genero);
+            Console.WriteLine($"\n\nMedida com oito casas decimais: {medida:F8}");
+            Console.WriteLine($"arredondado: {medida:F3}");
+            Console.WriteLine($"Separador Decimal: {medida.ToString("F3", CultureInfo.InvariantCulture)}");
+
+
+
+
             Console.WriteLine("completar depois.");
         }
     }
