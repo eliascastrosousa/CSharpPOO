11fb7e24125aba0d4c3e30cf810e50a4a7636a61
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-06-29 23:09:05 -0300

    commit exercicio1

---
+++ Ex1SaidaDeDados/Ex1SaidaDeDados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Ex1 {
+    class Ex1 {
+        static void Main(string[] args) {
+            Console.WriteLine("completar depois.");
+        }
+    }
+}
+
