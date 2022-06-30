f654cdada74b49056d2bae2691fd2d2aeaca2c1c
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-06-30 17:47:54 -0300

    casting

---
+++ Atv05Casting/Atv05Casting/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Atv05Casting {
+    internal class Program {
+        static void Main(string[] args) {
+            float x = 4.5f;
+            double y = x;
+            Console.WriteLine("Conversão implicita: {0}", y);
+
+            double a = 4.5;
+            float b = (float)a; //permitir Casting
+            Console.WriteLine("Casting: {0}", y);
+
+            int c = (int)a;
+            Console.WriteLine("Casting com Int: {0}",  c);
+
+            int d = 2;
+            int e = 3;
+            double f = (double)d/e;
+            Console.WriteLine("Casting com divis]ao de inteiros em double: {0}",f.ToString("F2"));
+
+
+
+
+            
+
+
+        }
+    }
+}
