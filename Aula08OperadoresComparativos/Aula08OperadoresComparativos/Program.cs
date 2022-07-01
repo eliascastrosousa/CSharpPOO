61ad834039c98c8bb4173f7f8b28b3e405b92110
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-06-30 21:00:48 -0300

    commit operadores comparativos

---
+++ Aula08OperadoresComparativos/Aula08OperadoresComparativos/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aula08OperadoresComparativos {
+    internal class Program {
+        static void Main(string[] args) {
+            int a = 10;
+            bool x = a < 20;
+            bool y = a > 30;
+            bool z = a == 20;
+            bool w = a != 20;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(w);
+
+
+            if (x){
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("Falso!!");
+            }
+        }
+    }
+}
