cd0164091d5cc37af284101e48fb6877ef2dca28
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-02 13:30:01 -0300

    commit aula10

---
+++ Aula10EstruturasCondicionais/Aula10EstruturasCondicionais/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Aula10EstruturasCondicionais {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite um numero: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+        }
+    }
+}
