4071eeeae48457f22bdc43d793274605bbf71bd0
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-02 17:03:18 -0300

    commit aula 11 funcoes

---
+++ Aula11Funcoes/Aula11Funcoes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Aula11Funcoes {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Digite 3 numeros: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            int resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("O Resultado é: " + resultado);
+
+        }
+
+        static int Maior(int a, int b, int c) {
+            int m = 0;
+
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > a && b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+
+            return m;
+        }
+
+
+
+
+    }
+}
