0ec6d9777ed88c44fb1e612049cf8d65af952ddc
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-04 22:56:22 -0300

    commit ex04 class funcionario

---
+++ Ex04_ClassFuncionario/Ex04_ClassFuncionario/Funcionario.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Ex04_ClassFuncionario {
+    public class Funcionario {
+        public string nome = null;
+        public double salario;
+    }
+}
---
+++ Ex04_ClassFuncionario/Ex04_ClassFuncionario/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex04_ClassFuncionario {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Funcionario Func_x, Func_y;
+
+            Func_x = new Funcionario();
+            Func_y = new Funcionario();
+
+            Console.Write("Digite o nome do Primeiro Funcionario: ");
+            Func_x.nome = Console.ReadLine();
+            Console.Write("Digite o Salario do Primeiro Funcionario: ");
+            Func_x.salario = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do Segundo Funcionario:  ");
+            Func_y.nome = Console.ReadLine();
+            Console.Write("Digite o Salario do Segundo Funcionario: ");
+            Func_y.salario = double.Parse(Console.ReadLine());
+
+            double media = (Func_x.salario + Func_y.salario) / 2;
+
+            Console.WriteLine("A media de Salarios é de: {0}", media);
+
+
+
+        }
+    }
+}
