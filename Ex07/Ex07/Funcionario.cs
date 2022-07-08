0998e0b35b973e93a31a29109ffbcacf3ae2c38a
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-07 22:34:26 -0300

    commit

---
+++ Ex07/Ex07/Funcionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex07 {
+    public class Funcionario {
+        public string nome;
+        public double salarioBruto;
+        public double imposto;
+
+        public void AumentoSalario(double porcentagem) {
+            porcentagem = salarioBruto * porcentagem / 1000;
+            salarioBruto = salarioBruto + porcentagem;
+        }
+
+        public double salarioLiquido() {
+            return salarioBruto - imposto;
+        }
+    }
+}
---
+++ Ex07/Ex07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Ex07 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Funcionario f;
+            f = new Funcionario();
+
+            Console.WriteLine("Digite o nome: ");
+            f.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o Salario bruto: ");
+            f.salarioBruto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do imposto: ");
+            f.imposto = double.Parse(Console.ReadLine());
+
+
+            double salLiq = f.salarioLiquido();
+            Console.WriteLine("Funcionario: {0}, Salario : {1}", f.nome, f.salarioLiquido());
+
+            Console.WriteLine("Digite a porcentagem de aumento do salario: ");
+            double aumento = double.Parse(Console.ReadLine());
+            //aumento = aumento / 10;
+
+            f.AumentoSalario(aumento);
+
+            Console.WriteLine("Funcionario: {0}, Salario : {1}", f.nome, f.salarioLiquido());
+
+
+        }
+    }
+}
