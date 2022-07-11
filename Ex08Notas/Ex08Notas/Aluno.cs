299ba80bdcd48d688169f6d227d1f54e5f2be95a
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-11 00:30:49 -0300

    commit exercicio notas

--- Ex08Notas/Ex08Notas/Aluno.cs
+++ Ex08Notas/Ex08Notas/Aluno.cs
@@ -5,6 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Ex08Notas {
-    internal class Aluno {
+    public class Aluno {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double nota3;
+
+        public double AlunoNotas() {
+            double n1 = ((nota1 * 0.30)/10);
+            double n2 = ((nota2 * 0.35) / 10);
+            double n3 = ((nota3 * 0.35) / 10);
+            return (n1 + n2 + n3)*10;
+        }
+
+
     }
+
+    
 }
--- Ex08Notas/Ex08Notas/Program.cs
+++ Ex08Notas/Ex08Notas/Program.cs
@@ -1,7 +1,43 @@
 ﻿namespace Ex08Notas {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            
+            Aluno al = new Aluno();
+            
+            Console.WriteLine("Digite seu nome: ");
+            al.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a nota do 1º Trimestre: ");
+            al.nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do 2º Trimestre: ");
+            al.nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do 3º Trimestre: ");
+            al.nota3 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("\n\nNome: {0}" +
+                              "\nNota 1: {1}" +
+                              "\nNota 2: {2}" +
+                              "\nNota 3: {3}", al.nome, al.nota1, al.nota2, al.nota3);
+
+            double notaFinal = al.AlunoNotas();
+
+            Console.WriteLine("Nota final: {0}", notaFinal);
+
+            if (notaFinal >= 6)
+            {
+                Console.WriteLine("Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado!");
+            }
+
+            
+
+
         }
     }
 }
