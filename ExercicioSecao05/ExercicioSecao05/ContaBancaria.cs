41221501b8af5a9d9aeabbcbc1da4562a0b99a7a
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-13 19:28:34 -0300

    commit exercicio secao5

---
+++ ExercicioSecao05/ExercicioSecao05/ContaBancaria.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExercicioSecao05 {
+    internal class ContaBancaria {
+
+
+        public string  Conta { get; private set; }
+        private string _nome;
+        private double _saldo;
+
+        public string Nome {
+            get {
+                return _nome;
+            }
+            set {
+                if (value.Length >= 2)
+                {
+                    _nome = value;
+                }
+                else
+                {
+                    Console.WriteLine("Não foi possivel Cadastrar o nome!");
+                }
+
+            }
+        }
+
+        public double Saldo {
+            get {
+                return _saldo;
+            }
+            set {
+                _saldo += value;
+            }
+        }
+
+        public ContaBancaria(string conta, string nome, double deposito) {
+            conta = Conta;
+            nome = Nome;
+            deposito = Saldo;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ ExercicioSecao05/ExercicioSecao05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExercicioSecao05 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            double deposito = 0;
+
+            Console.WriteLine("===========================");
+            Console.WriteLine("Bem vindo ao Banco CSharp ");
+            Console.WriteLine("===========================\n");
+
+            Console.Write("Digite o numero da Conta: ");
+            string numconta = Console.ReadLine();
+
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Deseja fazer deposito: ");
+            string x = Console.ReadLine();
+            if (x == "s" && x== "S" && x == "Sim" && x == "SIM" && x == "sim")
+            {
+                Console.Write("Digite o valor do deposito: ");
+                 deposito = double.Parse(Console.ReadLine());
+            }
+           
+            ContaBancaria C = new ContaBancaria(numconta, nome, deposito);
+
+            
+            Console.WriteLine(C);
+
+
+        }
+    }
+}
