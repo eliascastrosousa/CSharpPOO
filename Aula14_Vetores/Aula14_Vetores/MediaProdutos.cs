88310e099b2dff07815e256b71394faa293ba3bc
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-07-18 21:05:35 -0300

    commit ex

---
+++ Aula14_Vetores/Aula14_Vetores/MediaProdutos.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+
+namespace Aula14_Vetores {
+    internal class MediaProdutos {
+        public void MediaProduto() {
+            Console.Write("\nDigite a Quantidade de produtos que entrarão em estoque: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] produto = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write("Digite o Nome do Produto: ");
+                string Nome = Console.ReadLine();
+
+                Console.Write("Digite o Preço do Produto: ");
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                produto[i] = new Product { Name = Nome, Price = preco };
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nome do Produto [{0}]: {1}", i, produto[i].Name);
+                Console.WriteLine("Preço do Produto [{0}]: {1}", i, produto[i].Price);
+
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += produto[i].Price;
+            }
+            double avg = sum / n;
+
+            Console.WriteLine("\nSoma dos Valores: {0} \nMedia dos Valores: {1}\n", sum, avg);
+        }
+    }
+}
--- Aula14_Vetores/Aula14_Vetores/Program.cs
+++ Aula14_Vetores/Aula14_Vetores/Program.cs
@@ -2,8 +2,46 @@
 namespace Aula14_Vetores {
     internal class Program {
         static void Main(string[] args) {
-            Alturas alt = new Alturas();
-            alt.Altura();
+            int opc = 0;
+            do
+            {
+                Console.WriteLine("=============================================");
+                Console.WriteLine("Aula 14 - Vetores \nEscolha a opção de acordo com o que deseja: ");
+                Console.WriteLine("=============================================");
+
+                Console.WriteLine("\n[1]: Media de alturas utilizando Vetor");
+                Console.WriteLine("[2]: Media de Preços utilizando Vetor e classe Produto");
+                Console.Write("[0] Sair \nDigite: ");
+
+                opc = int.Parse(Console.ReadLine());
+
+                switch (opc)
+                {
+                    case 0:
+                        Console.WriteLine("\nSaindo...");
+                        break;
+                    case 1:
+                        Console.WriteLine("\nMedia de alturas utilizando Vetor");
+                        Alturas alt = new Alturas();
+                        alt.Altura();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nOpção 2: Media de Preços utilizando Vetor e classe Produto");
+                        MediaProdutos md = new MediaProdutos();
+                        md.MediaProduto();
+                        break;
+
+                    default:
+                        Console.WriteLine("\nOpção invalida");
+                        break;
+                }
+
+            } while (opc != 0);
+
+
+
+           
 
         }
     }
