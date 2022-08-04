8a1b72f3ae93cc30f39cb3280eb2ca2c40cf218a
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-08-04 14:44:58 -0300

    commit secao9

---
+++ Secao09/Secao09/Program.cs
@@ -0,0 +1,25 @@
+﻿using Secao09.Entities;
+using Secao09.Entities.Enums;
+
+
+namespace Secao09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            Console.WriteLine(order);
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+        }
+    }
+}
