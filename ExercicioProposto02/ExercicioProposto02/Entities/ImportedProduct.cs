f5c39fdc1fdbf3d88628d19536bbf7844187248b
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-08-17 15:01:43 -0300

    commit exercício proposto 2

---
+++ ExercicioProposto02/ExercicioProposto02/Entities/ImportedProduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace ExercicioProposto02.Entities 
+{
+    internal class ImportedProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportedProduct()
+        {
+
+        }
+
+        public ImportedProduct(String name, double price, double customsFee)
+            : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public override string PriceTag()
+        {
+            TotalPrice();
+            return base.PriceTag() + " (" + CustomsFee + ")".ToString();
+        }
+
+        public double TotalPrice()
+        {
+            Price = Price + CustomsFee;
+            return Price;
+        }
+
+        public override string ToString()
+        {
+            return Name + Price + CustomsFee.ToString();
+        }
+
+    }
+}
---
+++ ExercicioProposto02/ExercicioProposto02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using ExercicioProposto02.Entities;
+
+namespace ExercicioProposto02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of Products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Product #{0} Data: ", i + 1);
+
+                Console.Write("Product is Imported, Common or Used ? (I/C/U): ");
+                string opc = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine());
+
+                if (opc == "I")
+                {
+                    Console.Write("Customs Fee: ");
+                    double customsfee = double.Parse(Console.ReadLine());
+
+                    ImportedProduct importedProduct = new ImportedProduct(name, price, customsfee);
+                    products.Add(importedProduct);
+
+                }
+                else if(opc == "U")
+                {
+                    Console.Write("Manufacture Date (DD/MM/YYYY): ");
+                    DateTime manufacture = DateTime.Parse(Console.ReadLine());
+
+                    UsedProduct usedproduct = new UsedProduct(name, price, manufacture);
+                    products.Add(usedproduct);
+                }
+                else
+                {
+                    Product productCommum = new Product(name, price);
+                    products.Add(productCommum);
+                }
+            }
+            Console.WriteLine("\nPRICE TAGS: ");
+            foreach(Product product in products)
+            {
+                    Console.WriteLine(product.PriceTag());
+            }
+
+
+            
+
+
+            
+
+
+        }
+    }
+}
