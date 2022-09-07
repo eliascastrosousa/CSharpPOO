5d184e6e2b23146044fefbe71416e77d855482ff
eliascastrosousa <73971067+eliascastrosousa@users.noreply.github.com>
2022-09-07 15:50:23 -0300

    commit Other entities and second migration

--- SalesWebMvc/SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/SalesWebMvc/Models/Departament.cs
@@ -9,5 +9,27 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>(); //1 departamento muitos vendedores
+
+        public Departament()
+        {
+
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSeles(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
